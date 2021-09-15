4b8ed7936f42c88229ac7483c46c5372137cafd8
VinneyJ <vincentjayden49@gmail.com>
2021-09-15 17:56:03 +0300

    update export json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,35 +3,37 @@
 uses a REST API to return inforamtion about an employee given their
 employee ID
 """
-import json
-import requests
-import sys
+
 
 
 if __name__ == "__main__":
-''''
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    import json
+    import requests
+    import sys
+    
+    ''''
+        url = "https://jsonplaceholder.typicode.com/"
+        user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+        todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    #completed = [t.get("title") for t in todos if t.get("completed") is True]
-    #print("Employee {} is done with tasks({}/{}):".format(
-     #   user.get("name"), len(completed), len(todos)))
-    #[print("\t {}".format(c)) for c in completed]
+        #completed = [t.get("title") for t in todos if t.get("completed") is True]
+        #print("Employee {} is done with tasks({}/{}):".format(
+         #   user.get("name"), len(completed), len(todos)))
+        #[print("\t {}".format(c)) for c in completed]
 
-    u_id = user.get('id')
-    title = [t.get('title') for t in todo]
-    completed = todo.get('completed')
-    name = user.get('name')
+        u_id = user.get('id')
+        title = [t.get('title') for t in todo]
+        completed = todo.get('completed')
+        name = user.get('name')
 
-    content = {u_id: [{"task": title, "completed": completed, "username": name}]}
-    json_file = str(sys.argv[1]) + ".json"
-    with open(json_file, "w") as f:
-        if content:
-            f.write(str(content))
-        else:
-            print("content is empty")
-'''
+        content = {u_id: [{"task": title, "completed": completed, "username": name}]}
+        json_file = str(sys.argv[1]) + ".json"
+        with open(json_file, "w") as f:
+            if content:
+                f.write(str(content))
+            else:
+                print("content is empty")
+    '''
     
     
     url = "https://jsonplaceholder.typicode.com/"
