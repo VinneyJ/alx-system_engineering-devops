4a030ca56e983d14e193309b6dadce3382ef0610
VinneyJ <vincentjayden49@gmail.com>
2021-10-12 12:54:33 +0300

    pep8 update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {
-        "User-Agent" : "holberton"
+        "User-Agent": "holberton"
     }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
